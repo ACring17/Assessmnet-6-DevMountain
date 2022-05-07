31d72ebf1ce836eb8f91f836ef2eb9e4f7fac669
Andrew Cring <amcring1@gmail.com>
2022-05-07 11:17:11 -0700

    Adding everything that I have completed so far

---
+++ duelDuo.test.ts
@@ -0,0 +1,33 @@
+
+import { Builder, Capabilities, By } from "selenium-webdriver"
+
+require('chromedriver')
+
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+
+beforeEach(async () => {
+    driver.get('http://localhost:3000/')
+})
+
+afterAll(async () => {
+    await driver.sleep(1000)
+    await driver.quit()
+})
+
+test('Title shows up when page loads', async () => {
+    const title = await driver.findElement(By.id('title'))
+    const displayed = await title.isDisplayed()
+    expect(displayed).toBe(true)
+})
+// Should work to check the choices element
+test('The draw button displays the div', async () => {
+    const choices = await driver.findElement(By.id('draw'))
+    const displayed = await choices.isDisplayed()
+    expect(displayed).toBe(true)
+})
+//Should check that Add to Duo button displays the "player-duo" button
+test('Add to duo displays the div "player-duo', async () => {
+    const addDuo = await driver.findElement(By.id('player-duo'))
+    const displayed = await addDuo.isDisplayed()
+    expect(displayed).toBe(true)
+})
---
+++ functions.test.ts
@@ -0,0 +1,20 @@
+const {shuffleArray} = require('./utils')
+
+const testData = {
+    array : [1,2,3]
+}
+
+describe('shuffleArray should', () => {
+    test('return an array', () => {
+        const shuffledArray = shuffleArray(testData.array);
+
+        expect(typeof shuffledArray).toBe('object');
+    });
+
+    test('have same length', () => {
+        const shuffledArray = shuffleArray(testData.array) 
+        expect(shuffledArray.length).toBe(testData.array.length);
+    });
+});
+
+// Need to check that these tests pass
